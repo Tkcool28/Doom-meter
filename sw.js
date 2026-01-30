0abc608988c240c573d09e7a3c864bbd7fd00226
Tkcool28 <toddkirschman@gmail.com>
2026-01-30 00:00:54 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // - NEVER cache news/API responses
 // - Network-first for navigation + app.js so updates show up quickly
 
-const CACHE_NAME = "doomroom-static-v3.2.1";
+const CACHE_NAME = "doomroom-static-v3.2.2";
 
 const STATIC_ASSETS = [
   "./",
